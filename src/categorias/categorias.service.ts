49a1fbb318d15b18c36523b9f403bfb69d6d0525
Joao Sell <jpjpsell@gmail.com>
2026-04-04 16:49:50 -0300

    adding categoria routes

--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,1 +1,68 @@
-// ...service file for Categorias
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Categorias } from './entities/categorias.entity';
+import { CreateCategoriaDto } from './dto/createCategoria.dto';
+import { UpdateCategoriaDto } from './dto/updateCategoria.dto';
+
+@Injectable()
+export class CategoriasService {
+  constructor(
+    @InjectRepository(Categorias)
+    private categoriasRepository: Repository<Categorias>,
+  ) {}
+
+  /**
+   * Insere uma nova categoria.
+   * @param createCategoria - Dados da categoria.
+   * @returns Categoria criada.
+   */
+  async createCategoria(
+    createCategoria: CreateCategoriaDto,
+  ): Promise<Categorias> {
+    return this.categoriasRepository.save(createCategoria);
+  }
+
+  /**
+   * Atualiza uma categoria existente.
+   * @param id - ID do categoria.
+   * @param updateCategoria - Dados para atualização da categoria.
+   * @returns Categoria atualizada.
+   */
+  async updateCategoria(
+    id: number,
+    updateCategoria: UpdateCategoriaDto,
+  ): Promise<Categorias> {
+    const categoria = await this.getCategoriaById(id);
+
+    if (!categoria) {
+      throw new NotFoundException('Categoria não encontrada');
+    }
+
+    Object.assign(categoria, updateCategoria);
+
+    return this.categoriasRepository.save(categoria);
+  }
+
+  /**
+   * Retorna uma categoria.
+   * @param id - ID da categoria.
+   * @returns Categoria encontrada.
+   */
+  async getCategoriaById(id: number): Promise<Categorias | null> {
+    if (!id) {
+      throw new NotFoundException('Categoria não encontrada');
+    }
+    return this.categoriasRepository.findOne({ where: { id } });
+  }
+
+  /**
+   * Retorna todos os categorias registrados.
+   * @returns Lista de categorias.
+   */
+  async getAllCategorias(): Promise<Categorias[]> {
+    return this.categoriasRepository.find({
+      order: { id: 'ASC' },
+    });
+  }
+}
