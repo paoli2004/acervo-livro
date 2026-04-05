29653c825e44c443426f53b2f6db70a40c528ee5
pneto <pedronetopaoli@gmail.com>
2026-04-05 13:33:13 -0300

    AJUSTE EXEMPLARES

--- src/exemplares/dto/updateExemplar.dto.ts
+++ src/exemplares/dto/updateExemplar.dto.ts
@@ -1,11 +1,6 @@
-import { IsOptional, IsNumber } from 'class-validator';
+import { PartialType, PickType } from '@nestjs/mapped-types';
+import { CreateExemplarDto } from './createExemplar.dto';
 
-export class UpdateExemplarDto {
-  @IsNumber()
-  @IsOptional()
-  livro_id?: number;
-
-  @IsNumber()
-  @IsOptional()
-  codigo_patrimonio?: number;
-}
+export class UpdateExemplarDto extends PartialType(
+  PickType(CreateExemplarDto, ['livro_id', 'codigo_patrimonio'] as const),
+) {}
